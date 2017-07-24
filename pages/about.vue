c950bd1c9876c89fead693fb67118663fd9e617e
AM <amkungx@gmail.com>
2017-07-24 16:52:38 +0700

    fresh

---
+++ pages/about.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    about ทดสอบ {{item}}{{item2}}
+    <button class="btn btn-danger" @click="getData(1)">test</button>
+    <button class="btn btn-primary" @click="getData(2)">test</button>
+    <div class="fontThai">เทส</div>
+  </div>
+</template>
+<script>
+import axios from '~plugins/axios'
+export default {
+  data () {
+    return {
+      url: 'http://test.teachingtoys.co.th' + this.$route.path + '/',
+      item: { name: 'testname' },
+      item2: null
+    }
+  },
+  head () {
+    return {
+      title: this.item.name,
+      meta: [
+        { property: 'fb:app_id', content: '314810962296870' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.item.name },
+        { property: 'og:description', content: this.item.description },
+        { property: 'og:url', content: this.url },
+        { property: 'og:image', content: 'https://www.w3schools.com/css/img_fjords.jpg' }
+      ]
+    }
+  },
+  created () {
+    console.log(global.location)
+  },
+  asyncData () {
+    console.log('created!')
+    // var _self = this
+    return axios.get('http://localhost:8000/api/products/51')
+      .then((res) => {
+        return { item: res.data }
+      })
+  },
+  methods: {
+    getData (item) {
+      var _self = this
+      axios.get('http://localhost:8000/api/products/' + item)
+        .then((res) => {
+          _self.item2 = res.data
+        })
+    }
+  }
+}
+</script>
+<style>
+.fontThai {
+  font-family: 'Kanit';
+}
+</style>
+
